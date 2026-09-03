eff94dc660846b3a154cb8b4ebc3083bd140972a
Nishtha-Raut <rautnishtha29@gmail.com>
2026-09-03 17:40:31 +0530

    added comments in solution

--- Math/3876. Construct Uniform Parity Array II/solution.java
+++ Math/3876. Construct Uniform Parity Array II/solution.java
@@ -1,12 +1,19 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        int min=Integer.MAX_VALUE;
-        for(int i = 0; nums1.length>i;i++){
-            min=Math.min(nums1[i],min);
+        int min = Integer.MAX_VALUE;
+        // Find the minimum element
+        for (int i = 0; i < nums1.length; i++) {
+            min = Math.min(nums1[i], min);
         }
-        if(min%2!=0) return true;
-        for(int i=0;i<nums1.length;i++){
-            if(nums1[i]%2!=0) return false;
+        // If minimum is odd, we can make every element odd
+        if (min % 2 != 0) {
+            return true;
+        }
+        // If minimum is even, every element must already be even
+        for (int i = 0; i < nums1.length; i++) {
+            if (nums1[i] % 2 != 0) {
+                return false;
+            }
         }
         return true;
     }
